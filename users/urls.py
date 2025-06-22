21fbadcf09e1b698e8d41f63c9e07201e934a52b
reham <reehaam209@gmail.com>
2025-06-22 19:10:09 +0300

    feat: add user profile route & handle logout

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path , include
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import CustomLogoutView
+from users import views as user_views
+
 urlpatterns = [
     path('register',views.register,name="register"),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('profile',user_views.profile, name='profile'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     # search for why this logout is not accessing by /logout url 
     
 
